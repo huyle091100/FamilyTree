34147ca668c1ac60339981316afe76b42fc994cb
huyle091100 <lehuy091100@gmailcom>
2024-10-13 16:56:52 +0700

    Add Member

--- src/main/java/com/familytree/controllers/MemberController.java
+++ src/main/java/com/familytree/controllers/MemberController.java
@@ -3,10 +3,7 @@ package com.familytree.controllers;
 import com.familytree.models.Member;
 import com.familytree.services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,7 +29,12 @@ public class MemberController {
         }
         return theMember;
     }
-
-
+    // add
+    @PostMapping("/members")
+    public Member addMember(@RequestBody Member theMember) {
+        theMember.setId(1);
+        return memberService.saveMember(theMember);
+    }
+    
 }
 
